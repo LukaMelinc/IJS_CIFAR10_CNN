7421f76f306771e1a396f4d11c319d4f37cc17f7
LukaMelinc <luka.melinc1@gmail.com>
2024-03-06 15:43:15 +0100

    forward pass added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import torchvision.datasets as datasets
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import Module, Conv2d, Linear
-from torchvision import transforms#, datasets, models, ReLU, MaxPool2d, LogSoftmax
+from torchvision import transforms, datasets, models, ReLU, MaxPool2d, LogSoftmax
 import time
 import matplotlib.pyplot as plt
 import numpy
@@ -47,18 +47,45 @@ def adding_padding(image):
     plt.show()
     return padded_image_tensor
 
-"""# basic layer structure
+# basic layer structure
 class CNN(Module):
 	def __init__(self, num_channels, num_classes):
 		super(CNN, self).__init__()
-		# conv -> pool -> conv -> pool -> fc-> fc
+		# first set CONV -> RELU -> POOL
 		self.conv1 = Conv2d(in_channels=num_channels, out_channels=20,kernel_size=(5, 5)) # conv layer
 		self.relu1 = ReLU() #relu activation
 		self.maxpool1 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2)) # pooling layer
+		
+        # second set CONV -> RELU -> POOL
 		self.conv2 = Conv2d(in_channels=20, out_channels=50,kernel_size=(5, 5)) # conv layer 2
 		self.relu2 = ReLU() # relu layer 
 		self.maxpool2 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2)) #pooling layer 2
+		
+        # first and second fully connected layer with ReLU
 		self.fc1 = Linear(in_features=800, out_features=500) # fully connected layer 
 		self.relu3 = ReLU()
 		self.fc2 = Linear(in_features=500, out_features=num_classes) # fully connected layer 2
 		self.logSoftmax = LogSoftmax(dim=1) # softmax activation function"""
+		
+def forward(self, x):
+	# pass trough the first set 
+	#  CONV -> RELU -> POOL
+	x = self.conv1(x)
+	x = self.relu1(x)
+	x = self.maxpool1(x)
+	
+    # second pass
+	x = self.conv2(x)
+	x = self.relu2(x)
+	x = self.maxpool2(x)
+	
+    # flatten the output from previous sets
+	x = flatten(x, 1) # flatten into one dimension
+	x = self.fc1(x)
+	x = self.relu3(x)
+	
+    # pass trought the softmax classifier
+	x = self.fc2(x)
+	output = self.logSoftmax(x)
+	
+    return output
