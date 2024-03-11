4b6bfa08051b0fdea8635e2473f2837a5fd051ff
LukaMelinc <luka.melinc1@gmail.com>
2024-03-11 17:32:55 +0100

    dataset import fixed, def forward moved

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ import torchvision
 import torchvision.datasets as datasets
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn import ReLU, MaxPool2d
-from torch.nn import Module, Conv2d, Linear
-from torchvision import transforms, datasets, models
-from torch.utils.data import DataLoader, Dataset
 import time
 import matplotlib.pyplot as plt
 import numpy
+from torch.nn import ReLU, MaxPool2d, LogSoftmax
+from torch.nn import Module, Conv2d, Linear
+from torchvision import transforms, datasets, models
+from torch.utils.data import DataLoader, Dataset
 
 
 
-cifar_trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=None) # Transform ToTensor() 
 
-cifar_testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=None) # Transform ToTensor()
+
+cifar_trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transforms.ToTensor()) # Transform ToTensor() 
+cifar_testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transforms.ToTensor()) # Transform ToTensor()
 
 # train on GPU, if CUDA is available, else train on CPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  
@@ -48,6 +49,8 @@ testStep = len(testDataLoader.dataset)
     plt.imshow(cifar_testset[i][0], cmap= 'gray')
 plt.show()"""
 
+# conv2d definition
+# torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros', device=None, dtype=None)
 
 
 
@@ -68,43 +71,43 @@ class CNN(Module):
 		super(CNN, self).__init__()
 		# first set CONV -> RELU -> POOL
 		self.conv1 = Conv2d(in_channels=num_channels, out_channels=20,kernel_size=(5, 5)) # conv layer
-		self.relu1 = F.ReLU() #relu activation
+		self.relu1 = ReLU() #relu activation
 		self.maxpool1 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2)) # pooling layer
 		
         # second set CONV -> RELU -> POOL
 		self.conv2 = Conv2d(in_channels=20, out_channels=50,kernel_size=(5, 5)) # conv layer 2
-		self.relu2 = F.ReLU() # relu layer 
+		self.relu2 = ReLU() # relu layer 
 		self.maxpool2 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2)) #pooling layer 2
 		
         # first and second fully connected layer with ReLU
 		self.fc1 = Linear(in_features=800, out_features=500) # fully connected layer 
-		self.relu3 = F.ReLU()
+		self.relu3 = ReLU()
 		self.fc2 = Linear(in_features=500, out_features=num_classes) # fully connected layer 2
 		self.logSoftmax = LogSoftmax(dim=1) # softmax activation function"""
 		
-def forward(self, x):  # x represents a batch of input data to the network
-        # pass through the first set
-        #  CONV -> RELU -> POOL
-        x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.maxpool1(x)
-        
-        # second pass
-        # CONV -> RELU -> POOL
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.maxpool2(x)
-        
-        # flatten the output from previous sets
-        x = flatten(x, 1)  # flatten into 1D list
-        x = self.fc1(x)
-        x = self.relu3(x)
-        
-        # pass through the softmax classifier
-        x = self.fc2(x)
-        output = self.logSoftmax(x)
-        
-        return output
+	def forward(self, x):  # x represents a batch of input data flowing trough the network
+			# pass through the first set
+			#  CONV -> RELU -> POOL
+			x = self.conv1(x)
+			x = self.relu1(x)
+			x = self.maxpool1(x)
+			
+			# second pass
+			# CONV -> RELU -> POOL
+			x = self.conv2(x)
+			x = self.relu2(x)
+			x = self.maxpool2(x)
+			
+			# flatten the output from previous sets
+			x = flatten(x, 1)  # flatten into 1D list
+			x = self.fc1(x)
+			x = self.relu3(x)
+			
+			# pass through the softmax classifier
+			x = self.fc2(x)
+			output = self.logSoftmax(x)
+			
+			return output
 
 
 print("Start model CNN")
