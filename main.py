4d8cd7cb009fe0fcc21fa0add29f657f340843ad
LukaMelinc <luka.melinc1@gmail.com>
2024-03-31 16:26:40 +0200

    batch normalization added
    70% acc, still works like s***

--- main.py
+++ main.py
@@ -76,65 +76,56 @@ class CNN(Module):
 	def __init__(self, num_channels, num_classes):
 		super(CNN, self).__init__()
 		# first set CONV -> RELU -> POOL
-		self.conv1 = Conv2d(in_channels=num_channels, out_channels=num_channels,kernel_size=(5, 5)) # conv layer
-		self.relu1 = ReLU() #relu activation
-		self.maxpool1 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2)) # pooling layer
+		self.conv1 = Conv2d(in_channels=num_channels, out_channels=16, kernel_size=(5, 5))
+		self.bn1 = nn.BatchNorm2d(num_features=16)  # Corrected the argument here
+		self.relu1 = ReLU()
+		self.maxpool1 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
 		
-        # second set CONV -> RELU -> POOL
-		self.conv2 = Conv2d(in_channels=num_channels, out_channels=num_channels,kernel_size=(5, 5)) # conv layer 2
-		self.relu2 = ReLU() # relu layer 
-		self.maxpool2 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2)) #pooling layer 2
+		# second set CONV -> BatchNorm -> RELU -> POOL
+		self.conv2 = Conv2d(in_channels=16, out_channels=32, kernel_size=(5, 5))
+		self.bn2 = nn.BatchNorm2d(num_features=32)  # Ensure this matches the output channels of conv2
+		self.relu2 = ReLU()
+		self.maxpool2 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
 		
-		# Cifar stars with 3 x 32 x 32 dimensions (channels x width x height)
+		
 
-		# calculating in_features after conv and pool layers
-		# conv layer impact
-		# output size = (Inputsize - filter size(kernel size) + 2 * padding)/stride + 1
-		# default for padding and stride are 0 and 1
-
-		# 32x32 -> 28x28 as (32 - 5 + 1) = 28
-
-		# pooling layer impact
-		# output size = (input_size - pooling_size + 2 * padding)/stride + 1
-		# pooling_size = 2(2x2), padding = 0, stride = 2
-
-		# in_features = 5 x 5 x c; c = num_channels (RGB = 3, gray = 1)
-
-        # first and second fully connected layer with ReLU
-		self.fc1 = Linear(in_features=75, out_features=75) # fully connected layer 
+		# first and second fully connected layer with ReLU
+		self.fc1 = Linear(in_features=32*5*5, out_features=120) # fully connected layer 
 		self.relu3 = ReLU()
-		self.fc2 = Linear(in_features=75, out_features=75) # fully connected layer 2
+		self.fc2 = Linear(in_features=120, out_features=84) # fully connected layer 2
 		self.relu4 = ReLU()
-		self.fc3 = Linear(in_features=75, out_features=num_classes) # fully connected layer 3
+		self.fc3 = Linear(in_features=84, out_features=num_classes) # fully connected layer 3
 		self.logSoftmax = LogSoftmax(dim=1) # softmax activation function
 		
 	def forward(self, x):  # x represents a batch of input data flowing trough the network
-			# pass through the first set
-			#  CONV -> RELU -> POOL
-			x = self.conv1(x)
-			x = self.relu1(x)
-			x = self.maxpool1(x)
-			
-			# second pass
-			# CONV -> RELU -> POOL
-			x = self.conv2(x)
-			x = self.relu2(x)
-			x = self.maxpool2(x)
-			
-			# flatten the output from previous sets
-			x = flatten(x, 1)  # flatten into 1D list
-			x = self.fc1(x)
-			x = self.relu3(x)
-			
-			# pass through the softmax classifier
-			x = self.fc2(x)
-			x = self.relu4(x)
-			
-			# pass through the softmax classifier
-			x = self.fc3(x)
-			output = self.logSoftmax(x)
-			
-			return output
+		# pass through the first set
+		#  CONV -> RELU -> POOL
+		x = self.conv1(x)
+		x = self.bn1(x)
+		x = self.relu1(x)
+		x = self.maxpool1(x)
+		
+		# second pass
+		# CONV -> RELU -> POOL
+		x = self.conv2(x)
+		x = self.bn2(x)
+		x = self.relu2(x)
+		x = self.maxpool2(x)
+		
+		# flatten the output from previous sets
+		x = flatten(x, 1)  # flatten into 1D list
+		x = self.fc1(x)
+		x = self.relu3(x)
+		
+		# pass through the softmax classifier
+		x = self.fc2(x)
+		x = self.relu4(x)
+		
+		# pass through the softmax classifier
+		x = self.fc3(x)
+		output = self.logSoftmax(x)
+		
+		return output
 
 print("Start model CNN")
 # defining the training model
