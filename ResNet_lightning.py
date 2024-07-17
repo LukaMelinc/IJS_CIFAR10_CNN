ef0748fcebe048ce43fc73911d3c92fd2c2667ca
LukaMelinc <luka.melinc1@gmail.com>
2024-07-17 14:57:19 +0200

    pytorch lightning

--- ResNet_lightning.py
+++ ResNet_lightning.py
@@ -1,29 +1,24 @@
 import torch
 from torch import nn
-import torch.nn.functional as F
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader, random_split
 from torchvision.datasets import CIFAR10
 from torchvision import transforms
-import os
 from torch.optim import Adam
-
 from Models.ResNet_modular import ResNet, ResidualBlock
-from Testing import simple_test
-from DataLoading.CIFAR10_loader import get_data_loader
+from pytorch_lightning.loggers import TensorBoardLogger
 
 class LightningResNet(pl.LightningModule):
-
-    def __init__(self, learning_rate, weight_decay):
+    def __init__(self, learning_rate):
         super(LightningResNet, self).__init__()
         self.model = ResNet(ResidualBlock, [2,2,2,2])
         self.criterion = nn.CrossEntropyLoss()
         self.learning_rate = learning_rate
-        self.weight_decay = weight_decay
+        self.num_workers = num_workers
 
     def forward(self, x):
         return self.model(x)
-    
+
     def training_step(self, batch, batch_idx):
         images, labels = batch
         outputs = self(images)
@@ -34,7 +29,7 @@ class LightningResNet(pl.LightningModule):
         self.log('train_loss', loss, on_step=True, on_epoch=True)
         self.log('train_acc', accuracy, on_step=True, on_epoch=True)
         return loss
-    
+
     def validation_step(self, batch, batch_idx):
         images, labels = batch
         outputs = self(images)
@@ -42,18 +37,19 @@ class LightningResNet(pl.LightningModule):
         _, predicted = outputs.max(1)
         correct = (predicted == labels).sum().item()
         accuracy = correct / labels.size(0)
-        self.log('val_loss', loss, on_step=True, on_epoch=True)
-        self.log('val_acc', accuracy, on_step=True, on_epoch=True)
+        self.log('val_loss', loss, on_step=True, on_epoch=True, prog_bar=True)
+        self.log('val_acc', accuracy, on_step=True, on_epoch=True, prog_bar=True)
         return loss
-    
+
     def configure_optimizers(self):
-        optimizer = Adam(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
+        optimizer = Adam(self.parameters(), lr=self.learning_rate)
         return optimizer
-    
+
 class Cifar10DataModule(pl.LightningDataModule):
-    def __init__(self, batch_size):
+    def __init__(self, batch_size, num_workers=4):
         super().__init__()
         self.batch_size = batch_size
+        self.num_workers = num_workers
         self.transform = transforms.Compose([
                             transforms.ToTensor(),
                             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
@@ -85,36 +81,41 @@ class Cifar10DataModule(pl.LightningDataModule):
         self.cifar_test = cifar_testset
 
     def train_dataloader(self):
-        return DataLoader(self.cifar_train, batch_size=self.batch_size, shuffle=True)
+        return DataLoader(self.cifar_train, batch_size=self.batch_size, shuffle=True, num_workers = self.num_workers)
 
     def val_dataloader(self):
-        return DataLoader(self.cifar_val, batch_size=self.batch_size)
+        return DataLoader(self.cifar_val, batch_size=self.batch_size, num_workers = self.num_workers)
 
     def test_dataloader(self):
-        return DataLoader(self.cifar_test, batch_size=self.batch_size)
+        return DataLoader(self.cifar_test, batch_size=self.batch_size, num_workers = self.num_workers)
 
-from pytorch_lightning.loggers import TensorBoardLogger
+learning_rates = [0.005, 0.004, 0.003, 0.002, 0.001, 0.0009, 0.0008]
 
-learning_rates = [0.002, 0.001, 0.0009, 0.0008, 0.0007, 0.0006]
-weight_decays = [0, 0.01, 0.03, 0.05, 0.07, 0.1]
 batch_size = 64
-num_epochs = 10
+num_epochs = 15
+num_workers = 4
 
-cifar10_dm = Cifar10DataModule(batch_size=batch_size)
+cifar10_dm = Cifar10DataModule(batch_size=batch_size, num_workers=num_workers)
 
+# Perform grid search for hyperparameter tuning
 for lr in learning_rates:
-    for wd in weight_decays:
-        model = LightningResNet(learning_rate=lr, weight_decay=wd)
 
-        logger = TensorBoardLogger("runs/Tor_16_7", name=f"LR_{lr}_WD_{wd}")
+    # Initialize the model with the current set of hyperparameters
+    model = LightningResNet(learning_rate=lr)
 
-        trainer = pl.Trainer(
-            max_epochs=num_epochs,
-            gpus=1 if torch.cuda.is_available() else 0,
-            logger=logger,
-        )
+    # Initialize the TensorBoard logger
+    logger = TensorBoardLogger("runs/Tor_16_7/03", name=f"LR_{lr}")
 
-        trainer.fit(model, cifar10_dm)
-        trainer.test(model, cifar10_dm)
+    # Initialize the PyTorch Lightning trainer
+    trainer = pl.Trainer(
+        max_epochs=num_epochs,
+        accelerator='gpu' if torch.cuda.is_available() else 'cpu',
+        devices=1 if torch.cuda.is_available() else None,
+        logger=logger
+    )
 
+    # Train the model
+    trainer.fit(model, cifar10_dm)
     
+    # Test the model
+    trainer.test(model, datamodule=cifar10_dm)
